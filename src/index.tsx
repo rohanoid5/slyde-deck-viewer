9ee5304e1ff9c317a0fb8707c94a59e5a746692a
Rohan Dey <rohanoid5@gmail.com>
2021-11-14 02:13:35 +0530

    Added MUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "@mui/material/Button";
+
+import "./index.css";
 
 const HelloWorld = () => {
-    const message: string = 'Hello, World!!!';
+  const message: string = "Hello, World!!!";
 
-    return (
-        <h1>
-            {message}
-        </h1>
-    );
-}
+  return (
+    <div>
+      <h1>{message}</h1>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+};
 
 ReactDOM.render(<HelloWorld />, document.getElementById("root"));
-

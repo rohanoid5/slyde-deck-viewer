352fdde29db78617e9ec279d097d9e24bdabbb05
Rohan Dey <rohanoid5@gmail.com>
2021-11-15 01:50:35 +0530

    Added default theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import Button from "@mui/material/Button";
+import { Container, createTheme, CssBaseline, Typography } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
+
+import { defaultThemeOptions } from "./theme/default";
 
 import "./index.css";
 
 const HelloWorld = () => {
   const message: string = "Hello, World!!!";
 
+  const theme = createTheme(defaultThemeOptions);
+
   return (
-    <div>
-      <h1>{message}</h1>
-      <Button variant="contained">Hello World</Button>
+    <div className="main">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container sx={{ my: 8 }}>
+          <h1>{message}</h1>
+          <Button variant="contained">Hello World</Button>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 };
---
+++ src/theme/default.ts
@@ -0,0 +1,14 @@
+import { ThemeOptions } from "@mui/material/styles/createTheme";
+
+export const defaultThemeOptions: ThemeOptions = {
+  palette: {
+    primary: {
+      main: "#0f0",
+    },
+    background: {
+      default: "#111111",
+      paper: "#212121",
+    },
+    mode: "dark",
+  },
+};
